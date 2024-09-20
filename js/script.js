4f621682bea27d237cee47ac1b75800fff1f22a6
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-09-20 13:26:51 +0200

    feat: add new resume

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const pdf = require('html-pdf-node');
 
-fs.readFile('D:\\Projects\\resume-builder\\templates\\index.html', 'utf8', (err, htmlContent) => {
+fs.readFile('D:\\Projects\\resume-builder\\templates\\html,css.html', 'utf8', (err, htmlContent) => {
   if (err) {
     console.error("Error read file", err);
     return;
@@ -12,7 +12,7 @@ fs.readFile('D:\\Projects\\resume-builder\\templates\\index.html', 'utf8', (err,
   let options = { format: 'A4' };
 
   pdf.generatePdf(file, options).then(pdfBuffer => {
-    fs.writeFileSync("D:\\Projects\\resume-builder\\output\\Fullstack.pdf", pdfBuffer);
+    fs.writeFileSync("D:\\Projects\\resume-builder\\output\\HTML,CSS.pdf", pdfBuffer);
     console.log("PDF saved successfully");
   }).catch(error => {
     console.error("Error create PDF", error);
